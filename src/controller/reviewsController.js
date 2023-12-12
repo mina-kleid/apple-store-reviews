5417ad878e83ca344a34a9e49542c6c0555551e6
Mina Kleid <minazakiz@gmail.com>
2023-12-12 08:15:38 +0000

    add pagination

--- src/controller/reviewsController.js
+++ src/controller/reviewsController.js
@@ -3,9 +3,23 @@ import { fetchReviews, updateReviews } from '../service/reviews.js'
 export async function getReviews(req, res) {
   try {
     const { appId } = req.params
-    const reviews = await fetchReviews(appId)
+    const allReviews = await fetchReviews(appId)
+    let { page = 1, limit = 10 } = req.query;
 
-    res.json({ reviews });
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const reviews = allReviews.slice(startIndex, endIndex);
+    const totalItems = allReviews.length;
+    const totalPages = Math.ceil(totalItems / limit);
+
+    const pagination = {
+      totalItems,
+      itemsPerPage: reviews.length,
+      currentPage: parseInt(page),
+      totalPages
+    };
+
+    res.json({ reviews, pagination });
   } catch (error) {
     if (error.code === 'ENOENT') {
       res.json({ reviews: [] });
