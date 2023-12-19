adceeb6766162e8c5a05d8d9d38f4a8ed3e34193
Mina Kleid <minazakiz@gmail.com>
2023-12-19 16:37:13 +0000

    fixing CORS issue

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import reviewRoutes from './routes.js'
+import cors from 'cors'
 import 'dotenv/config'
 
 const app = express()
 const PORT = 3000
+
+const corsOptions = {
+  origin: 'http://localhost:8080'
+}
+
+app.use(cors(corsOptions))
 
 app.use('/app', reviewRoutes)
 
